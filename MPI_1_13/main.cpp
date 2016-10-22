396a460738c758af06404874ce53e1e9e1f6e750
Semen Bevzuk <semen.bevzuk@gmail.com>
2016-10-22 23:48:46 +0300

    Коллективный сбор на нулевом потоке

--- MPI_1_13/main.cpp
+++ MPI_1_13/main.cpp
@@ -10,7 +10,7 @@ int* CreateAndFillMatrixVector(int n, int m) {
 	int i;
 	int* matrix = (int*)malloc(n * m * sizeof(int));
 	for (i = 0; i < n*m; i++) {
-		matrix[i] = 0 + rand() % 1000;
+		matrix[i] = 0 + rand() % 100;
 	}
 	return matrix;
 }
@@ -57,7 +57,7 @@ void PrintVector(int *data, int dataSize) {
 
 using namespace std;
 int main(int argc, char* argv[]) {
-	double time1, time2, delta_time_1, delta_time_2;
+	double time1, time2, delta_time_parallel, delta_time_consistent;
 	MPI_Status status;
 	FILE *f=NULL;
 	int i;
@@ -67,7 +67,10 @@ int main(int argc, char* argv[]) {
 	int thread_count, rank;
 	int dataSize, bufferSize;
 	int deltaSize;
-	int MaxNumber;
+	int LocalMax;
+	int remainingData;
+	int GlobalMax;
+	int res;
 
 	if (argc >= 3) {
 		n = atoi(argv[1]);
@@ -82,101 +85,99 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	dataSize = (n*m) / (thread_count - 1); //элементов на поток
-	deltaSize = n*m - dataSize*(thread_count - 1); //добавка к стандартному размеру полоски
-
-	bufferSize = dataSize;
-	if (rank < (deltaSize + 1) && rank != 0) {
-		bufferSize = dataSize + 1;
+	remainingData = n*m - dataSize*(thread_count-1); //добавка к стандартному размеру полоски
+	if (rank==0){
+		bufferSize = remainingData;
 	}
+	else{
+		bufferSize = dataSize;
+	}
+	vector = new int[bufferSize];
 
-	if (rank == 0) {
+	if (rank == 0){
 		matrix = CreateAndFillMatrixVector(n, m);
-		printf_s("n = %d, m = %d\n", n, m);
-
-		if (n*m<=100)
-		{
+		//printf_s("n = %d, m = %d\n", n, m);
+		//printf_s("bufferSize = %d, remainingData = %d\n", dataSize, remainingData);
+		if (n*m <= 100) {
 			PrintMatrixVector(matrix, n, m);
 		}
-
 		time1 = MPI_Wtime();
-		vector = new int[thread_count];
 
 		int *temp_start_matrix = matrix;
 
 		for (i = 1; i < thread_count; i++) {
-			if (i<deltaSize + 1) {
-				MPI_Send(temp_start_matrix, bufferSize + 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-				temp_start_matrix = temp_start_matrix + bufferSize + 1;
-			}
-			else {
-				MPI_Send(temp_start_matrix, bufferSize, MPI_INT, i, 0, MPI_COMM_WORLD);
-				temp_start_matrix = temp_start_matrix + bufferSize;
-			}
+			MPI_Send(temp_start_matrix, dataSize, MPI_INT, i, 0, MPI_COMM_WORLD);
+			temp_start_matrix = temp_start_matrix + dataSize;
 		}
-		for (i = 1; i < thread_count; i++) {
-			int number;
-			MPI_Recv(&number, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
-			vector[status.MPI_SOURCE] = number;
-		}
-		MaxNumber = vector[1];
-		for (i = 1; i < thread_count; i++) {
-			if (vector[i]>MaxNumber) {
-				MaxNumber = vector[i];
+		if (remainingData!=0) {
+			LocalMax = matrix[dataSize*(thread_count - 1)];
+			for (i = dataSize*(thread_count - 1)+1; i < n*m; i++) {
+				if (matrix[i]>LocalMax)
+				{
+					LocalMax = matrix[i];
+				}
 			}
 		}
-		time2 = MPI_Wtime();
+	}
 
-		delta_time_1 = time2 - time1;
+	if (rank!=0) {
+		MPI_Recv(vector, bufferSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		LocalMax = vector[0];
+		for (i = 0; i < bufferSize; i++) {
+			if (vector[i]>LocalMax) {
+				LocalMax = vector[i];
+			}
+		}
+	}
+
+	MPI_Reduce(&LocalMax, &GlobalMax, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+
+	if (rank == 0)
+	{
+		time2 = MPI_Wtime();
+		delta_time_parallel = time2 - time1;
 		printf_s("MPI:\n");
-		printf_s("	Time = %f\n", delta_time_1);
+		printf_s("	Time = %f\n", delta_time_parallel);
 		printf_s("	Threads = %d\n", thread_count);
-		printf_s("	Max = %d\n\n", MaxNumber);
-		if (delta_time_1 > EPS) {
+		printf_s("	GlobalMax = %d\n\n", GlobalMax);
+		if (delta_time_parallel > EPS) {
 			fopen_s(&f, "../../log/parallel.txt", "a");
-			fprintf_s(f, "%f %d %d %d\n", delta_time_1, n, m, thread_count);
+			fprintf_s(f, "%f %d %d %d\n", delta_time_parallel, n, m, thread_count);
 			fflush(f);
 			fclose(f);
 		}
-	}
-	else {
-		vector = new int[bufferSize];
-		MPI_Recv(vector, bufferSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-		int MaxNumber = vector[0];
-		for (i = 0; i < bufferSize; i++) {
-			if (vector[i]>MaxNumber) {
-				MaxNumber = vector[i];
-			}
-		}
-		//printf_s("rank = %d; buffer = %d; Max Number = %d; \n", rank, bufferSize, MaxNumber);
-		//PrintVector(vector, bufferSize);
-		MPI_Send(&MaxNumber, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 	}
 
 	if (rank == 0) {
 		time1 = MPI_Wtime();
-		MaxNumber = FindMaxInMatrix(matrix, n, m);
+		GlobalMax = FindMaxInMatrix(matrix, n, m);
 		time2 = MPI_Wtime();
 
-		delta_time_2 = time2 - time1;
+		delta_time_consistent = time2 - time1;
 		printf_s("Сonsistent implementation:\n");
-		printf_s("	Time = %f\n", delta_time_2);
-		printf_s("	Max = %d\n", MaxNumber);
-		if (delta_time_2>EPS) {
+		printf_s("	Time = %f\n", delta_time_consistent);
+		printf_s("	GlobalMax = %d\n", GlobalMax);
+		if (delta_time_consistent>EPS) {
 			fopen_s(&f, "../../log/consistent.txt", "a");
-			fprintf(f, "%f %d %d\n", delta_time_2, n, m);
+			fprintf(f, "%f %d %d\n", delta_time_consistent, n, m);
 			fflush(f);
 			fclose(f);
 		}
 
-		printf_s("\nOptimal choice: ", MaxNumber);
-		if (delta_time_1<delta_time_2)
+		fopen_s(&f, "../../log/result.txt", "a");
+		printf_s("\nOptimal choice: ");
+		if (delta_time_parallel<delta_time_consistent)
 		{
-			printf_s("parallel implementation.\n");
+			printf_s("parallel implementation. Delta_time = %f;\n", delta_time_consistent - delta_time_parallel);
+			fprintf(f, "Parallel implementation;  DeltaTime = %f; Threads = %d; n = %d; m = %d;\n", delta_time_consistent - delta_time_parallel, thread_count, n, m);
 		}
 		else
 		{
-			printf_s("consistent implementation.\n");
+			printf_s("consistent implementation. Delta_time = %f;\n", delta_time_parallel - delta_time_consistent);
+			fprintf(f, "Consistent implementation; DeltaTime = %f; Threads = %d; n = %d; m = %d;\n", delta_time_parallel - delta_time_consistent, thread_count, n, m);
 		}
+		fflush(f);
+		fclose(f);
 		DeleteMatrix(matrix);
 	}
 	free(vector);
